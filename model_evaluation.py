f7c5bd638f30458599b16e28c6a25cf2a6fa6855
Axel Gemin - CdP <axel.gemin@reseau.eseo.fr>
2025-11-21 10:09:34 -0600

    added misclassified test_cases exploration function

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 
@@ -18,3 +19,20 @@ def make_model_graph(y_pred: pd.Series):
     plt.tight_layout()
     plt.savefig("data/predicted_sentiment_counts.png", dpi=200, bbox_inches="tight")
     print("Saved plot: predicted_sentiment_counts.png")
+
+
+def print_missclassified_test_cases(
+    X_test: pd.Series, y_test: pd.Series, y_pred: pd.Series
+):
+    misclassified_mask = y_pred != y_test
+    misclassified_indices = np.where(misclassified_mask)[0]
+
+    total_errors = len(misclassified_indices)
+
+    print(f"MISCLASSIFIED TEST CASES ({total_errors} errors:)")
+
+    for i, idx in enumerate(misclassified_indices, 1):
+        print(f"\n{i}. Review: {X_test.iloc[idx]}")
+        print(f"   True: {y_test.iloc[idx]} | Predicted: {y_pred[idx]}")
+
+    print("\n" + "=" * 100)
